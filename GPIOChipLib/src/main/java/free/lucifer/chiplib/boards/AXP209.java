ea0eb0eea008c2e1c1eba1128b7ae54e8ff586dd
lucifer <dandreev@at-consulting.ru>
2016-12-04 01:35:05 +0600

    License

--- GPIOChipLib/src/main/java/free/lucifer/chiplib/boards/AXP209.java
+++ GPIOChipLib/src/main/java/free/lucifer/chiplib/boards/AXP209.java
@@ -1,8 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2016 Denis Andreev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package free.lucifer.chiplib.boards;
 
 import free.lucifer.chiplib.Chip;
@@ -10,10 +21,7 @@ import free.lucifer.chiplib.Chip.Pin;
 import free.lucifer.chiplib.modules.I2C;
 import java.util.concurrent.locks.ReentrantLock;
 
-/**
- *
- * @author lucifer
- */
+
 public class AXP209 implements IOBoard {
 
     private static final byte BUTTON_REGISTER = 0x4a;
--- GPIOChipLib/src/main/java/free/lucifer/chiplib/boards/PCF8574A.java
+++ GPIOChipLib/src/main/java/free/lucifer/chiplib/boards/PCF8574A.java
@@ -1,8 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2016 Denis Andreev.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package free.lucifer.chiplib.boards;
 
 import free.lucifer.chiplib.Chip.Pin;
@@ -16,10 +27,6 @@ import static free.lucifer.chiplib.Chip.Pin.XIO_P6;
 import static free.lucifer.chiplib.Chip.Pin.XIO_P7;
 import free.lucifer.chiplib.modules.I2C;
 
-/**
- *
- * @author lucifer
- */
 public class PCF8574A implements IOBoard {
 
     private I2C i2c;
